ea54ea9904c37ffbfd8bdcf5229f164098c61edc
Agustin-Venegas <68612141+Agustin-Venegas@users.noreply.github.com>
2021-10-09 15:47:12 -0300

    Interface
    Interfaz de ejemplo
    Las interfaces sirven para herencia multiple

---
+++ Project N/Assets/Scripts/Interfaces/IHurtable.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Todo lo que es dañable implementa esto
+public interface IHurtable
+{
+    bool IsAlive(); //debe regresar si sigue con vida
+
+    bool Hurt(int d); //que hace con el damage recibido. debe regresar si muere.
+
+    void Die(); //que pasa al morir
+}
