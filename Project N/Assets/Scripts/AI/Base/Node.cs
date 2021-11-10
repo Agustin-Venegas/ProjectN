63058036a918d1eb98b2adee066f32683eb3f42b
Agustin-Venegas <68612141+Agustin-Venegas@users.noreply.github.com>
2021-11-10 02:15:31 -0300

    BehaviourTree
    Se sumaron las clases
    Node
    NodeStates
    Selector
    
    En su respectivo espacio de nombres

---
+++ Project N/Assets/Scripts/AI/Base/Node.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace BehaviourTree
+{
+	
+	//nodo generico. Todos los nodos heredan de esto
+	
+	[System.Serializable]
+	public abstract class Node
+	{
+		
+		protected NodeStates actualState;
+		
+		public delegate NodeStates Return();
+		
+		public Node() {}
+		
+		public abstract NodeStates Evaluar();
+		
+	}
+}
---
+++ Project N/Assets/Scripts/AI/Base/Selector.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BehaviourTree
+{
+	//Selector: Ejecuta los hijos hasta que uno regrese SUCCESS
+	//Si ninguno tira SUCCESS, regresa FAILURE
+	
+	public class Selector : Node
+	{
+		protected List<Node> nodes = new List<Node>();
+		
+		public Selector(List<Node> n) 
+		{
+			nodes = n;
+		}
+		
+		public override NodeStates Evaluar() 
+		{ 
+			foreach (Node node in nodes)
+			{ 
+				switch (node.Evaluar())
+				{ 
+                case NodeStates.FAILURE: 
+                    continue;
+                case NodeStates.SUCCESS: 
+                    actualState = NodeStates.SUCCESS; 
+                    return actualState; 
+                case NodeStates.RUNNING: 
+                    actualState = NodeStates.RUNNING; 
+                    return actualState; 
+                default: 
+                    continue; 
+				} 
+			} 
+			actualState = NodeStates.FAILURE; 
+			return actualState; 
+		} 
+	}
+}
